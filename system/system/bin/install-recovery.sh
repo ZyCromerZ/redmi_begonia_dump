a1034b902cee19c718e32efd0b85bb1e0d25191e
ZyCromerZ <neetroid97@gmail.com>
2020-10-21 21:44:29 +0700

    Add system for begonia-user 10 QP1A.190711.020 20.10.20 release-keys
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

---
+++ system/system/bin/install-recovery.sh
@@ -0,0 +1,11 @@
+#!/system/bin/sh
+if ! applypatch --check EMMC:/dev/block/platform/bootdevice/by-name/recovery:67108864:d0ac16ff14b95b49273e063b08356f9a842d66d7; then
+  applypatch  \
+          --patch /system/recovery-from-boot.p \
+          --source EMMC:/dev/block/platform/bootdevice/by-name/boot:67108864:4aef258f044e73aebd846617c82529ca1c36c39f \
+          --target EMMC:/dev/block/platform/bootdevice/by-name/recovery:67108864:d0ac16ff14b95b49273e063b08356f9a842d66d7 && \
+      log -t recovery "Installing new recovery image: succeeded" || \
+      log -t recovery "Installing new recovery image: failed"
+else
+  log -t recovery "Recovery image already installed"
+fi
